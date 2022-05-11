6ebbf82a9c572eb4750a2ea0a8e1b31ccb121fa6
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-11 09:55:39 +0800

    update comment

--- frontend/app/src/main/java/cn/edu/tsinghua/zhouhang/liuyihao/thubbs/ui/components/MyCircleImageView.java
+++ frontend/app/src/main/java/cn/edu/tsinghua/zhouhang/liuyihao/thubbs/ui/components/MyCircleImageView.java
@@ -36,38 +36,38 @@ public class MyCircleImageView extends MyImageView {
 
     private void init() {
         mPaint = new Paint();
-        mPaint.setAntiAlias(true);//给画笔设置抗锯齿
+        mPaint.setAntiAlias(true); // 给画笔设置抗锯齿
         mMatrix = new Matrix();
-        //该方法千万别放到onDraw()方法里面调用，否则会不停的重绘的，因为该方法调用了invalidate() 方法
+        // 该方法千万别放到onDraw()方法里面调用，否则会不停的重绘的，因为该方法调用了invalidate() 方法
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);//禁用硬加速
     }
 
-    //重写onDraw()方法获取BitmapDrawable进行处理
+    // 重写onDraw()方法获取BitmapDrawable进行处理
     @Override
     protected void onDraw(Canvas canvas) {
         Drawable drawable = getDrawable();
         if (drawable instanceof BitmapDrawable) {
             BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
             Bitmap bitmap = bitmapDrawable.getBitmap();
-            //通过BitmapShader的方式实现
+            // 通过BitmapShader的方式实现
             drawRoundByShaderMode(canvas, bitmap);
-            //super.onDraw(canvas);
+            // super.onDraw(canvas);
         } else {
             super.onDraw(canvas);
         }
     }
 
     private void drawRoundByShaderMode(Canvas canvas, Bitmap bitmap) {
-        //获取到Bitmap的宽高
+        // 获取到Bitmap的宽高
         int bitmapWidth = bitmap.getWidth();
         int bitmapHeight = bitmap.getHeight();
 
-        //获取到ImageView的宽高
+        // 获取到ImageView的宽高
         int viewWidth = getWidth();
         int viewHeight = getHeight();
 
-        //根据Bitmap生成一个BitmapShader，这里有个TileMode设置有三个参数可以选择：
-        //CLAMP, MIRROR, REPEAT 在后面会细说。
+        // 根据Bitmap生成一个BitmapShader，这里有个TileMode设置有三个参数可以选择：
+        // CLAMP, MIRROR, REPEAT 在后面会细说。
         BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
         mMatrix.reset();
 
@@ -78,7 +78,7 @@ public class MyCircleImageView extends MyImageView {
 
         bitmapWidth *= minScale;
         bitmapHeight *= minScale;
-        //绘制圆形
+        // 绘制圆形
         canvas.drawCircle((float) bitmapWidth / 2, (float) bitmapHeight / 2, Math.min(bitmapWidth / 2, bitmapHeight / 2), mPaint);
     }
 }
