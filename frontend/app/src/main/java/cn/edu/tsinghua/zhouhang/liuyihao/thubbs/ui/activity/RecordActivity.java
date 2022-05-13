7e0d4caa4f571ed75ef1ad0bd5bafc7d3a358343
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-13 19:17:47 +0800

    换用aac编码格式的音频

--- frontend/app/src/main/java/cn/edu/tsinghua/zhouhang/liuyihao/thubbs/ui/activity/RecordActivity.java
+++ frontend/app/src/main/java/cn/edu/tsinghua/zhouhang/liuyihao/thubbs/ui/activity/RecordActivity.java
@@ -13,8 +13,6 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -27,13 +25,7 @@ import com.luck.lib.camerax.utils.DensityUtil;
 import com.luck.lib.camerax.widget.CaptureButton;
 import com.luck.lib.camerax.widget.TypeButton;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Calendar;
 
 import cn.edu.tsinghua.zhouhang.liuyihao.thubbs.Constant;
@@ -42,19 +34,14 @@ import cn.edu.tsinghua.zhouhang.liuyihao.thubbs.databinding.ActivityRecordBindin
 import cn.edu.tsinghua.zhouhang.liuyihao.thubbs.utils.Alert;
 
 public class RecordActivity extends AppCompatActivity {
-    private final static int AUDIO_INPUT = MediaRecorder.AudioSource.MIC;
-    private final static int AUDIO_SAMPLE_RATE = 44100;
-    private final static int AUDIO_CHANNEL = AudioFormat.CHANNEL_IN_MONO; // 单声道
-    private final static int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
     private ActivityRecordBinding binding;
     private int layoutWidth;
     private CaptureButton btnCapture;
     private TypeButton btnConfirm;
     private TypeButton btnCancel;
-    private AudioRecord mAudioRecorder;
-    private int mBufferSize = 0;
-    private boolean isRecording = false;
+    private MediaRecorder mMediaRecorder;
+    private String _mAudioUrl = null;
     private String mAudioUrl = null;
 
     @Override
@@ -191,8 +178,10 @@ public class RecordActivity extends AppCompatActivity {
                     Constant.RECORD_PERMISSION);
             return;
         }
-        mBufferSize = AudioRecord.getMinBufferSize(AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING);
-        mAudioRecorder = new AudioRecord(AUDIO_INPUT, AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING, mBufferSize);
+        mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
     }
 
     private void atBefore() {
@@ -231,116 +220,19 @@ public class RecordActivity extends AppCompatActivity {
     }
 
     private void startRecord() {
-        String dirUrl = getDir(Constant.TMP_DIR, MODE_PRIVATE).getPath();
-        String fileUrl = dirUrl + "/" + Constant.RAW_WAV;
-        String outputFileUrl = dirUrl + "/AUD_" + Calendar.getInstance().getTime().getTime() + ".wav";
-        new Thread(() -> {
-            File file = new File(fileUrl);
-            if (file.exists()) {
-                if (!file.delete()) {
-                    Alert.error(this, R.string.unknown_error);
-                    return;
-                }
-            }
-            try {
-                if (!file.createNewFile()) {
-                    Alert.error(this, R.string.unknown_error);
-                    return;
-                }
-            } catch (IOException ioe) {
-                Alert.error(this, R.string.unknown_error);
-                return;
-            }
-            try {
-                OutputStream os = new FileOutputStream(file);
-                BufferedOutputStream bos = new BufferedOutputStream(os);
-                byte[] buffer = new byte[mBufferSize];
-                mAudioRecorder.startRecording();
-                isRecording = true;
-                while (isRecording) {
-                    int res = mAudioRecorder.read(buffer, 0, mBufferSize);
-                    if (res > 0) {
-                        bos.write(buffer, 0, mBufferSize);
-                    }
-                }
-                mAudioRecorder.stop();
-                bos.close();
-                os.close();
-            } catch (IOException ioe) {
-                Alert.error(this, R.string.unknown_error);
-                return;
-            }
-            try {
-                FileInputStream in = new FileInputStream(file);
-                FileOutputStream out = new FileOutputStream(outputFileUrl);
-                long totalFileLen = in.getChannel().size();
-                long totalDataLen = totalFileLen + 44 - 8;
-                writeWavHeader(out, totalFileLen, totalDataLen);
-                byte[] buffer = new byte[mBufferSize];
-                while (in.read(buffer) > 0) {
-                    out.write(buffer);
-                }
-                in.close();
-                out.close();
-            } catch (IOException ioe) {
-                Alert.error(this, R.string.unknown_error);
-                return;
-            }
-            mAudioUrl = outputFileUrl;
-        }).start();
+        _mAudioUrl = getDir(Constant.TMP_DIR, MODE_PRIVATE).getPath() +
+                "/AUD_" + Calendar.getInstance().getTime().getTime() + ".aac";
+        mMediaRecorder.setOutputFile(_mAudioUrl);
+        try {
+            mMediaRecorder.prepare();
+            mMediaRecorder.start();
+        } catch (IOException ioe) {
+            Alert.error(this, R.string.unknown_error);
+        }
     }
 
     private void stopRecord() {
-        isRecording = false;
-    }
-
-    private void writeWavHeader(OutputStream out, long totalFileLen, long totalDataLen) throws IOException {
-        byte[] header = new byte[44];
-        header[0] = 'R'; // RIFF / WAVE header
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        header[4] = (byte) (totalDataLen & 0xff);
-        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-        header[8] = 'W';
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        header[12] = 'f'; // 'fmt' chunk
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        header[16] = 16;
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        header[20] = 1; // WAV type format = 1
-        header[21] = 0;
-        header[22] = (byte) 1; // 指示是单声道还是双声道
-        header[23] = 0;
-        header[24] = (byte) (AUDIO_SAMPLE_RATE & 0xff); // 采样频率
-        header[25] = (byte) ((AUDIO_SAMPLE_RATE >> 8) & 0xff);
-        header[26] = (byte) ((AUDIO_SAMPLE_RATE >> 16) & 0xff);
-        header[27] = (byte) ((AUDIO_SAMPLE_RATE >> 24) & 0xff);
-        header[28] = (byte) (AUDIO_SAMPLE_RATE & 0xff); // 每分钟录到的字节数
-        header[29] = (byte) ((AUDIO_SAMPLE_RATE >> 8) & 0xff);
-        header[30] = (byte) ((AUDIO_SAMPLE_RATE >> 16) & 0xff);
-        header[31] = (byte) ((AUDIO_SAMPLE_RATE >> 24) & 0xff);
-        header[32] = (byte) (header[22] * 16 / 8); // block align（声道数 * 量化位数 / 8）
-        header[33] = 0;
-        header[34] = 16; // bits per sample
-        header[35] = 0;
-        header[36] = 'd';
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        header[40] = (byte) (totalFileLen & 0xff); //真实数据的长度
-        header[41] = (byte) ((totalFileLen >> 8) & 0xff);
-        header[42] = (byte) ((totalFileLen >> 16) & 0xff);
-        header[43] = (byte) ((totalFileLen >> 24) & 0xff);
-        // 把 header 写入 wav 文件
-        out.write(header, 0, 44);
+        mMediaRecorder.stop();
+        mAudioUrl = _mAudioUrl;
     }
 }
